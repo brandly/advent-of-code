821193f78aa550d2e74480eef97002daa4d4337b
brandly <brandly13@gmail.com>
2017-12-01 11:40:30 -0500

    1.2

--- 2017/1.js
+++ 2017/1.js
@@ -22,9 +22,33 @@ function captcha (nums) {
   }, 0)
 }
 
-assert.ok(captcha([1, 1, 2, 2]) === 3)
-assert.ok(captcha([1, 1, 1, 1]) === 4)
-assert.ok(captcha([1, 2, 3, 4]) === 0)
-assert.ok(captcha([9, 1, 2, 1, 2, 1, 2, 9]) === 9)
+assert.equal(captcha([1, 1, 2, 2]), 3)
+assert.equal(captcha([1, 1, 1, 1]), 4)
+assert.equal(captcha([1, 2, 3, 4]), 0)
+assert.equal(captcha([9, 1, 2, 1, 2, 1, 2, 9]), 9)
 
 console.log(captcha(input))
+
+function captcha2 (nums) {
+  return nums.reduce((matches, value, index, list) => {
+    const nextIndex = (list.length / 2 + index) % list.length
+    const next = list[nextIndex]
+
+    if (next === value) {
+      return matches.concat(value)
+    } else {
+      return matches
+    }
+  }, [])
+  .reduce((total, val) => {
+    return total + val
+  }, 0)
+}
+
+assert.equal(captcha2([1, 2, 1, 2]), 6)
+assert.equal(captcha2([1, 2, 2, 1]), 0)
+assert.equal(captcha2([1, 2, 3, 4, 2, 5]), 4)
+assert.equal(captcha2([1, 2, 3, 1, 2, 3]), 12)
+assert.equal(captcha2([1, 2, 1, 3, 1, 4, 1, 5]), 4)
+
+console.log(captcha2(input))
