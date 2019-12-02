96fdcb4eb491dbde114ceec55a4ea33e248937c5
brandly <brandly13@gmail.com>
2019-12-02 17:15:39 -0500

    2019/01

---
+++ 2019/01.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const assert = require('assert')
+
+const nums = fs.readFileSync('./2019/01.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map(n => parseInt(n))
+
+const fuel = n => Math.floor(n / 3) - 2
+
+const sum = list =>
+  list.reduce((a, b) => a + b, 0)
+
+console.log('pt 1:', sum(nums.map(fuel)))
+
+const withFuelWeight = weight => {
+  const additional = fuel(weight)
+
+  if (additional > 0) {
+    return weight + withFuelWeight(additional)
+  } else {
+    return weight
+  }
+}
+
+const part2 = a => withFuelWeight(fuel(a))
+
+assert(part2(14) === 2)
+assert(part2(1969) === 966)
+assert(part2(100756) === 50346)
+
+console.log('pt 2:', sum(nums.map(part2)))
